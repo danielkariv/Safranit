1f39fafd6cf4c1dd9512613458982e17b2e257e8
Daniel <danielkariv98@gmail.com>
2024-11-01 02:19:02 +0200

    wip design (with daisy ui)

---
+++ safranit-website/src/app/page.tsx
@@ -0,0 +1,316 @@
+"use client";
+import { useState } from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          {/* Page content here */}
+          <div className="navbar bg-base-100 lg:hidden">
+            <div className="flex-none">
+              <label
+                htmlFor="my-drawer-2"
+                className="btn btn-primary drawer-button lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block h-5 w-5 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
+            </div>
+            <div className="flex-1 lg:hidden">
+              <a className="btn btn-ghost text-xl">Safranit</a>
+            </div>
+          </div>
+          <div className="flex flex-grow p-4">
+            <div className="w-full content-start flex flex-wrap justify-center gap-6">
+              <div className="card glass w-96">
+                <figure>
+                  <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="car!"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">Life hack</h2>
+                  <p>How to park your car at your garage?</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Learn now!</button>
+                  </div>
+                </div>
+              </div>
+              <div className="card glass w-96">
+                <figure>
+                  <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="car!"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">Life hack</h2>
+                  <p>How to park your car at your garage?</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Learn now!</button>
+                  </div>
+                </div>
+              </div>
+              <div className="card glass w-96">
+                <figure>
+                  <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="car!"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">Life hack</h2>
+                  <p>How to park your car at your garage?</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Learn now!</button>
+                  </div>
+                </div>
+              </div>
+              <div className="card glass w-96">
+                <figure>
+                  <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="car!"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">Life hack</h2>
+                  <p>How to park your car at your garage?</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Learn now!</button>
+                  </div>
+                </div>
+              </div>
+              <div className="card glass w-96">
+                <figure>
+                  <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="car!"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">Life hack</h2>
+                  <p>How to park your car at your garage?</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Learn now!</button>
+                  </div>
+                </div>
+              </div>
+              <div className="card glass w-96">
+                <figure>
+                  <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="car!"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">Life hack</h2>
+                  <p>How to park your car at your garage?</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Learn now!</button>
+                  </div>
+                </div>
+              </div>  
+            </div>
+          </div>
+        </div>
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer-2"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <div className="menu bg-base-200 text-base-content min-h-full w-80 flex flex-col">
+            <ul className="p-2 flex-grow">
+              {/* Sidebar content here */}
+              <li>
+                <div className="p-4 flex justify-between items-center">
+                  <a className="text-xl">Safranit</a>
+                  <label
+                    htmlFor="my-drawer-2"
+                    aria-label="close sidebar"
+                    className="btn btn-ghost btn-xs btn-circle lg:hidden"
+                  >
+                    <svg
+                      data-v-9ba4cb7e=""
+                      data-v-8d292eb9=""
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M18 6 6 18M6 6l12 12"
+                      ></path>
+                    </svg>
+                  </label>
+                </div>
+              </li>
+              <li>
+                <ul className="p-2">
+                  <li>
+                    <a className="flex items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                        <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                      </svg>
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a className="flex items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+                      </svg>
+                      Wish List
+                    </a>
+                  </li>
+                  <li>
+                    <a className="flex items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                      </svg>
+                      Search
+                    </a>
+                  </li>
+                  <li>
+                    <a className="flex items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <line x1="4" y1="9" x2="20" y2="9"></line>
+                        <line x1="4" y1="15" x2="20" y2="15"></line>
+                        <line x1="10" y1="3" x2="8" y2="21"></line>
+                        <line x1="16" y1="3" x2="14" y2="21"></line>
+                      </svg>
+                      My Books
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+
+            <footer className="footer footer-center bg-primary text-primary-content p-8 justify-end">
+              <aside>
+                <svg
+                  width="50"
+                  height="50"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  className="inline-block fill-current"
+                >
+                  <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
+                </svg>
+                <p className="font-bold">
+                  Safranit
+                  <br />
+                  Catalog Your Reads, Celebrate Your Journey
+                </p>
+                <p>
+                  Copyright © {new Date().getFullYear()} - All right reserved
+                </p>
+              </aside>
+              <nav>
+                <div className="grid grid-flow-col gap-4">
+                  <a>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      className="fill-current"
+                    >
+                      <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+                    </svg>
+                  </a>
+                  <a>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      className="fill-current"
+                    >
+                      <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+                    </svg>
+                  </a>
+                  <a>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      className="fill-current"
+                    >
+                      <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+                    </svg>
+                  </a>
+                </div>
+              </nav>
+            </footer>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
