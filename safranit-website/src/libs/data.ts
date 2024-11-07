021af48b89d70b9234e22be6c2ba08fb203724d5
Daniel <danielkariv98@gmail.com>
2024-11-07 16:37:21 +0200

    add books api, fix let to const

--- safranit-website/src/libs/data.ts
+++ safranit-website/src/libs/data.ts
@@ -12,10 +12,13 @@ export type Book = {
   };
   
 
-export async function fetchBookData() {
+export async function fetchBookData(id : string) {
     try{
-        const data = await sql<Book>`SELECT * FROM books`;
-        let row = data.rows[0];
+        const data = await sql<Book>`SELECT * FROM books WHERE id=${id}`;
+        if (data.rowCount == 0)
+            return null;
+        
+        const row = data.rows[0];
         return {
             href: `/book/${row.id}/`,
             imageSrc: row.image,
@@ -26,7 +29,7 @@ export async function fetchBookData() {
         };
     } catch (error) {
         console.error('Database Error:', error);
-        throw new Error('Failed to fetch revenue data.');
+        throw new Error(`Failed to fetch book data, id:${id}.`);
       }
   }
 
@@ -35,7 +38,7 @@ export async function fetchLastestBookData() {
         const data = await sql<Book>`SELECT * FROM books
                                     ORDER BY last_updated DESC
                                     LIMIT 1;`;
-        let row = data.rows[0];
+        const row = data.rows[0];
         return {
             href: `/book/${row.id}/`,
             imageSrc: row.image,
@@ -47,7 +50,7 @@ export async function fetchLastestBookData() {
         };
     } catch (error) {
         console.error('Database Error:', error);
-        throw new Error('Failed to fetch revenue data.');
+        throw new Error('Failed to fetch lastest book data.');
       }
   }
 
@@ -56,7 +59,7 @@ export async function fetchLastestBookData() {
         const data = await sql<Book>`SELECT * FROM books
                                     ORDER BY last_updated DESC
                                     LIMIT ${count} OFFSET ${page-1};`;
-        let books: { href: string; imageSrc: string; title: string; writer: string; des: string; badges: string[]; storelinks:JSON }[] = []
+        const books: { href: string; imageSrc: string; title: string; writer: string; des: string; badges: string[]; storelinks:JSON }[] = []
         data.rows.forEach((row) => {
             // Assuming row is an object with book data
             books.push({
@@ -72,6 +75,28 @@ export async function fetchLastestBookData() {
         return books;
     } catch (error) {
         console.error('Database Error:', error);
-        throw new Error('Failed to fetch revenue data.');
+        throw new Error(`Failed to fetch lastest books, page:${page} count:${count}.`);
+      }
+  }
+  
+
+export async function insertBook(
+    imageSrc: string,
+    title: string,
+    writer: string,
+    des: string,
+    badges: Array<string>,
+    storelinks: JSON) {
+    
+    try{
+        const query = `
+        INSERT INTO books (image, title, writer, tags, description, storeLinks)
+        VALUES ($1, $2, $3, $4, $5, $6);`;
+      
+        // Parameters are passed as an array, which automatically prevents SQL injection
+        await sql.query(query, [imageSrc, title, writer, badges, des, storelinks]);
+    } catch (error) {
+        console.error('Database Error:', error);
+        throw new Error('Failed to insert new book data.');
       }
   }
